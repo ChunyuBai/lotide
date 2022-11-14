c2c6639cd204f2b130d82a4a164584d8e4a40212
ChunyuBai <baichunyu26@gmail.com>
2022-11-14 18:51:37 -0500

    my assertion

---
+++ assertEqual.js
@@ -0,0 +1,16 @@
+// FUNCTION IMPLEMENTATION
+
+const assertEqual = function(actual, expected) {
+  
+  console.assert(actual === expected, `${actual} !== ${expected} 🛑🛑🛑`);
+  if (actual === expected) {
+    console.log(`Assertion passed:`, `${actual} === ${expected} ✅✅✅`);
+  }
+
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 0);
+assertEqual("wow","wow");
+assertEqual(2,2);
