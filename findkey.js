3ff7aba485c0e0f9ae49c5ca80071ba9006892be
ChunyuBai <baichunyu26@gmail.com>
2022-11-17 17:02:01 -0500

    create findKey function

---
+++ findkey.js
@@ -0,0 +1,35 @@
+const assertEqual = function(actual, expected) { 
+  console.assert(actual === expected, `${actual} !== ${expected} 🛑🛑🛑`);
+  if (actual === expected) {
+    console.log(`Assertion passed:`, `${actual} === ${expected} ✅✅✅`);
+  }
+};
+
+const findKey = (obj,callBack) => {
+  // console.table(obj)
+  for(Key in obj){
+    // console.log(obj[Key].stars);
+    if(callBack(obj[Key])){
+      return Key;
+    } 
+  }
+  return undefined;
+  }
+
+console.log(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2)) // => "noma"
+
+console.log(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 4)) // undefined;
